6ec4aa7efca1ddf52bd14157ec41302f7e08330b
qhy <724769318@qq.com>
2023-02-28 19:39:55 +0800

    添加可移动的僵尸ai

---
+++ Source/RPGGame/Public/MyBTTask_ZombieAttack.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "MyBTTask_ZombieAttack.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RPGGAME_API UMyBTTask_ZombieAttack : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+};
